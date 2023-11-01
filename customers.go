a2efb939e5cbe45ada0d9ec099e1db58a75f2784
David Mendoza <jdmendozar@gmail.com>
2023-11-01 11:03:25 -0500

    customers

---
+++ customers.go
@@ -0,0 +1,148 @@
+package main
+
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+
+	"github.com/google/uuid"
+	"github.com/gorilla/mux"
+)
+
+type Customer struct {
+	ID   string `json:"id"`
+	Name string `json:"name"`
+}
+
+func getCustomers(w http.ResponseWriter, r *http.Request) {
+	log.Println("getting customers")
+	rows, err := db.Query("SELECT id, name FROM customers order by name")
+	if err != nil {
+		log.Println(err)
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	defer rows.Close()
+
+	customers := []*Customer{}
+	for rows.Next() {
+		customer := &Customer{}
+		err := rows.Scan(&customer.ID, &customer.Name)
+		if err != nil {
+			log.Println(err)
+			w.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+		customers = append(customers, customer)
+	}
+
+	result, err := json.Marshal(customers)
+	if err != nil {
+		log.Println(err)
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	w.WriteHeader(http.StatusOK)
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(result)
+}
+
+func getCustomer(w http.ResponseWriter, r *http.Request) {
+	log.Println("getting customer")
+	vars := mux.Vars(r)
+	id := vars["customerID"]
+	if id == "" {
+		log.Println("missing customerID")
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	row := db.QueryRow("SELECT id, name FROM customers WHERE id = $1", id)
+
+	customer := &Customer{}
+	err := row.Scan(&customer.ID, &customer.Name)
+	if err != nil {
+		log.Println(err)
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	result, err := json.Marshal(customer)
+	if err != nil {
+		log.Println(err)
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	w.WriteHeader(http.StatusOK)
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(result)
+}
+
+func addCustomer(w http.ResponseWriter, r *http.Request) {
+	log.Println("adding customer")
+	customer := &Customer{}
+	err := json.NewDecoder(r.Body).Decode(customer)
+	if err != nil {
+		log.Println(err)
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	customer.ID = uuid.New().String()
+	_, err = db.Exec("INSERT INTO customers (id, name) VALUES ($1, $2)", customer.ID, customer.Name)
+	if err != nil {
+		log.Println(err)
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	result, err := json.Marshal(customer)
+	if err != nil {
+		log.Println(err)
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	w.WriteHeader(http.StatusCreated)
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(result)
+}
+
+func updateCustomer(w http.ResponseWriter, r *http.Request) {
+	log.Println("updating customer")
+	customer := &Customer{}
+	err := json.NewDecoder(r.Body).Decode(customer)
+	if err != nil {
+		log.Println(err)
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	_, err = db.Exec("UPDATE customers SET name = $1 WHERE id = $2", customer.Name, customer.ID)
+	if err != nil {
+		log.Println(err)
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	w.WriteHeader(http.StatusOK)
+}
+
+func deleteCustomer(w http.ResponseWriter, r *http.Request) {
+	log.Println("deleting customer")
+	vars := mux.Vars(r)
+	id := vars["customerID"]
+	if id == "" {
+		log.Println("missing customerID")
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	_, err := db.Exec("DELETE FROM customers WHERE id = $1", id)
+	if err != nil {
+		log.Println(err)
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	w.WriteHeader(http.StatusOK)
+}
--- main.go
+++ main.go
@@ -1,8 +1,35 @@
 package main
 
-import "log"
+import (
+	"database/sql"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+	_ "github.com/jackc/pgx/v5/stdlib"
+)
+
+var db *sql.DB
 
 func main() {
 	log.SetFlags(log.LstdFlags | log.Lshortfile)
-	log.Println("Hello World!")
+	log.Println("Connecting to database")
+	var err error
+	db, err = sql.Open("pgx", "postgres://jdmr:test@localhost:5432/orders?sslmode=disable")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	log.Println("Creating new router")
+	router := mux.NewRouter()
+
+	log.Println("Registering routes")
+	router.HandleFunc("/customers", getCustomers).Methods("GET")
+	router.HandleFunc("/customers", addCustomer).Methods("POST")
+	router.HandleFunc("/customers/{customerID}", getCustomer).Methods("GET")
+	router.HandleFunc("/customers/{customerID}", updateCustomer).Methods("PUT")
+	router.HandleFunc("/customers/{customerID}", deleteCustomer).Methods("DELETE")
+
+	log.Println("Starting server on port 8080")
+	log.Fatal(http.ListenAndServe(":8080", router))
 }
